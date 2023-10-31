abe9587c1988978e19bd187fc313a799e2633ca7
Update56 <heupdate56@gmail.com>
2023-10-31 22:36:41 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+
+namespace Alg_lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long x = 1;
+            while (x != 0) {
+                Console.Write("x = ");
+                x = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine($"Ответ: f = {f(x)}");
+            }
+        }
+        static long f(long x)
+        {
+            int z = 0;
+            while (!(x + 10 < Math.Pow(2,z+1)) && z <= x + 10) 
+                z++;
+            return z;
+        }
+    }
+}
